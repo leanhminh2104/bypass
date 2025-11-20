bc415e56f497d9bfcd53824c6605cf6e3961e2e4
Lê Anh Minh <lamdev2104@gmail.com>
2025-11-20 19:29:40 +0700

    Update auto-bypass-link4m.user.js

--- auto-bypass-link4m.user.js
+++ auto-bypass-link4m.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
-// @name         Auto Bypass Link4m + Link2m
-// @description  Menu quản lý Auto Bypass Link4m + Link2m
+// @name         Auto Bypass Link4m
+// @description  Menu quản lý Auto Bypass Link4m
 // @require      https://raw.githubusercontent.com/leanhminh2104/bypass/refs/heads/link4m/auto-bypass-link4m-v1.5.js
 // @updateURL    https://raw.githubusercontent.com/leanhminh2104/bypass/refs/heads/link4m/auto-bypass-link4m-v1.5.js
 // @downloadURL  https://raw.githubusercontent.com/leanhminh2104/bypass/refs/heads/link4m/auto-bypass-link4m-v1.5.js
@@ -8,7 +8,6 @@
 // @version      1.5
 // @author       LAMDev - dichvusale.io.vn
 // @match        *://link4m.com/go/*
-// @match        *://2m.com/go/*
 // @grant        none
 // @icon         https://i.postimg.cc/3JQz6v9Y/logo-tr-n.png
 // ==/UserScript==
