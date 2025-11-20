5ef435d6806720427aabec73cfffdab4114f8606
Lê Anh Minh <lamdev2104@gmail.com>
2025-11-20 12:33:22 +0700

    Add Auto Bypass Link4m user script

---
+++ auto-bypass-link4m.user.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         Auto Bypass Link4m + Link2m
+// @description  Menu quản lý Auto Bypass Link4m + Link2m
+// @require      https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/speep-wed-by-lamdev-v3.js
+// @updateURL   https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/speep-wed-by-lamdev-v3.js
+// @downloadURL https://raw.githubusercontent.com/leanhminh2104/Speep-Wed-By-LAMDev/refs/heads/main/speep-wed-by-lamdev-v3.js
+// @namespace    http://tampermonkey.net/
+// @version      1.5
+// @author       LAMDev - dichvusale.io.vn
+// @match        *://*/*
+// @grant        none
+// @icon         https://i.postimg.cc/3JQz6v9Y/logo-tr-n.png
+// ==/UserScript==
